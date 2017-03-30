83aa2b22be1eb0775b755956fd30335fa2ca5046
Dylan Cordoni <email": null>
2017-03-30 13:59:20 -0700

    Adds: player controller with routes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,6 @@
 class PlayersController < ApplicationController
+  before_action :find_player, only [:show, :edit, :update]
+
   def index
   end
 
@@ -6,6 +8,13 @@ class PlayersController < ApplicationController
   end
 
   def create
+    @player = Player.new(params[:player])
+    if @player.save
+      redirect_to user_path(@playear)
+    else
+      @errors = @player.errors.full_messages
+      render :new
+    end
   end
 
   def show
@@ -15,5 +24,15 @@ class PlayersController < ApplicationController
   end
 
   def update
+    @players.update_attributes(params[:player])
+    redirect_to root_url
   end
+
+  private
+    def find_player
+      @player.find(params[:player_id])
+
+    def player_params
+      params.require(:player).permit(:username, :email, :password)
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def new
-    render :new
   end
 
   def create
