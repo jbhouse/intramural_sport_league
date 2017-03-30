a8d065ffe33f88f7af7bcd6fb481202f968eec4b
Dylan Cordoni <email": null>
2017-03-30 12:16:34 -0700

    Adds: user auth to user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @user = User.authenticate(params[:username], params[:password])
+    @user = Player.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_url
     else
