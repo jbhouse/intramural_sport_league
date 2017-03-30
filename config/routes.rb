bfe72f2f1231ef04e9a4daf654149c1c2e9a2274
Joe Moorhouse <jbmoorhouse1@gmail.com>
2017-03-30 12:05:36 -0700

    added link_to's to the layouts page and added sessions as a resource in the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
     resources :teams, except: [:destroy], shallow: true
   end
   resources :roster_entries, only: [:create, :new, :destroy]
-  resources :games, except: [:destroy, :index] 
+  resources :games, except: [:destroy, :index]
   resources :players, except: [:destroy] do
     resources :rsvps, only: [:create, :new, :destroy],shallow: true
   end
+  resources :sessions, only: [:create, :new, :destroy]
+
+  root to: 'sports#index'
 
 end
