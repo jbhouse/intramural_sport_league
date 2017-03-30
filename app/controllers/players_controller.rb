428d5df0496b5abe99ecc0d829ddcbc86098bf2e
Dylan Cordoni <email": null>
2017-03-30 13:59:58 -0700

    Fixes: bug in before_action in player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 class PlayersController < ApplicationController
-  before_action :find_player, only [:show, :edit, :update]
+  before_action :find_player, only: [:show, :edit, :update]
 
   def index
   end
