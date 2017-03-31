07688fc0097a7da9281230c577458d605e05e849
Christopher Huynh & Jesse Calton <pair+chhuynh.jessecalton@devbootcamp.com>
2017-03-30 18:05:43 -0700

    fixed sport show page logic to match new migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,12 +49,12 @@ RosterEntry.create(sub?: true, player_id: 4, team_id: 1)
   )
 }
 
+
 5.times {Game.create(
   location: Faker::StarWars.planet,
   date: Faker::Date.forward(10),
   time: "6:30 PM",
   away_team_id: 2,
-  home_team_id: 3,
-  winning_team: 3
+  home_team_id: 3
   )
 }
